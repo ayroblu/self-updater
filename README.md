fca0b2f4ae7e656c3a39249b7ed87f37f1bcd5cb
Ben Lu <ayroblu@gmail.com>
2020-12-14 17:02:54 +0000

    Make it work

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ Self updater
 ============
 
 A simple node service that updates docker files
+
+```sh
+rsync -aPvv index.js deploy.sh site:/var/www/self-updater/
+```
+
+For the systemd config
+
+```sh
+rsync -aPvv updater.service site:/lib/systemd/system/
+ssh systemctl daemon-reload
+```
--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    if (req.url.contains("ez47xqwefklh5iidjbsg")) {
-      if (req.url.contains("aklbuses.nz")) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+    if (req.url.includes("ez47xqwefklh5iidjbsg")) {
+      if (req.url.includes("aklbuses.nz")) {
         execSync(`bash ${__dirname}/deploy.sh aklbuses.nz`, {
+          stdio: "inherit",
+        });
+      } else if (req.url.includes("ayro.nz")) {
+        execSync(`bash ${__dirname}/deploy.sh ayro.nz`, {
+          stdio: "inherit",
+        });
+      } else if (req.url.includes("portfolio")) {
+        execSync(`bash ${__dirname}/deploy.sh portfolio`, {
           stdio: "inherit",
         });
       }
     }
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
   })
   .listen(process.env.PORT || "8080");
